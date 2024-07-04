a08f1adaaf888f7b0f9bb2a5adcec6ab445b6dde
Simlex <simlexafol@gmail.com>
2024-07-04 11:59:50 +0100

    update url

--- app/api/apiRoutes.ts
+++ app/api/apiRoutes.ts
@@ -11,12 +11,12 @@ export class ApiRoutes {
   /**
    * The test base url for the application
    */
-  static BASE_URL_TEST: string = "https://buffy-service.onrender.com";
+  static BASE_URL_TEST: string = "https://buffy-clicker.netlify.app/";
 
   /**
    * The live base url for the application
    */
-  static BASE_URL_LIVE: string = "https://buffy-service.onrender.com";
+  static BASE_URL_LIVE: string = "https://buffy-clicker.netlify.app/";
 
   /**
    * The route to Users endpoint
